9d16611a1247b8ed01c383b70734d8b4261a934d
pdhoward <patrick.howard@hotmail.com>
2015-04-10 14:57:46 -0400

    This adds a new version of server.js
    This adds a new version of the server.js file

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-    res.send('Hello from inside LIQUIDXIO container!');
+    res.send('Hello from inside LIQUIDXIO container! This is version 4');
 });
 
 app.listen(8080);
